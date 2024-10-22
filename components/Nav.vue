58c97623f08ce0925d26890e679a3b9d3a6cb88b
Tadeo Ramirez <l20121026@morelia.tecnm.mx>
2024-10-21 18:24:52 -0600

    revisiones

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,45 +1,136 @@
 <template>
   <nav class="border-primary-light bg-gray-950 md:bg-black fixed w-full top-0 left-0 shadow-md z-50">
-    <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4 shadow-2xl"> <!-- Cambiado a max-w-screen-2xl -->
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 shadow-2xl">
       <!-- Logo -->
       <NuxtLink href="/" class="flex items-center">
         <img src="/images/LOGO LETRAS-.png" class="h-16" alt="LA BRU Logo" />
       </NuxtLink>
 
       <!-- Menu Button for Mobile -->
-      <button @click="toggleMenu" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <button @click="toggleMenu" data-collapse-toggle="navbar-default" type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
 
-      <!-- Menu Items -->
-      <div :class="isMenuOpen ? 'block w-full slide-in' : 'hidden' + ' md:block md:w-auto'" id="navbar-default">
-        <ul class="flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0">
+      <!-- Menu Items and Social Icons for Desktop -->
+      <div class="hidden md:flex md:items-center">
+        <ul class="flex space-x-8">
           <li>
-            <a href="#about" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">Who We Are</a>
+            <a href="#about" class="text-white hover:text-primary">Who We Are</a>
           </li>
           <li>
-            <a href="#beers" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">Lager</a>
+            <a href="#beers" class="text-white hover:text-primary">Lager</a>
           </li>
           <li>
-            <a href="#story" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">Cream Ale</a>
+            <a href="#story" class="text-white hover:text-primary">Cream Ale</a>
           </li>
           <li>
-            <a href="#find" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">IPA</a>
+            <a href="#find" class="text-white hover:text-primary">IPA</a>
           </li>
           <li>
-            <a href="#find" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">Storytelling</a>
+            <a href="#find" class="text-white hover:text-primary">Storytelling</a>
           </li>
           <li>
-            <a href="#find" class="block py-2 px-4 text-right text-gray-900 hover:bg-primary-light md:hover:bg-transparent md:hover:text-primary md:p-0 dark:text-white">Ubication</a>
+            <a href="#find" class="text-white hover:text-primary">Ubication</a>
           </li>
         </ul>
+        <!-- Social Icons for Desktop -->
+        <div class="flex items-center ml-8 space-x-4">
+          <NuxtLink href="#" class="text-white hover:text-primary flex items-center justify-center w-10 h-10">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+              <g fill="#ff8000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                style="mix-blend-mode: normal">
+                <g transform="scale(8,8)">
+                  <path
+                    d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z">
+                  </path>
+                </g>
+              </g>
+            </svg>
+          </NuxtLink>
+          <NuxtLink href="#" class="text-white hover:text-primary flex items-center justify-center w-7 h-7">
+            <svg fill="#ff8000" height="100%" width="100%" version="1.1" id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 310 310"
+              xml:space="preserve">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <g id="XMLID_834_">
+                  <path id="XMLID_835_"
+                    d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064 c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996 V71.978c0-9.732,5.24-14.667,15.576-14.667c1.473,0,29.42,0,29.42,0c2.762,0,5-2.239,5-5V5.037c0-2.762-2.238-5-5-5h-40.545 C187.467,0.023,186.832,0,185.896,0c-7.035,0-31.488,1.381-50.804,19.151c-21.402,19.692-18.427,43.27-17.716,47.358v37.752H81.703 c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z">
+                  </path>
+                </g>
+              </g>
+            </svg>
+          </NuxtLink>
+        </div>
+      </div>
+
+      <!-- Mobile Menu -->
+      <div :class="isMenuOpen ? 'block w-full slide-in' : 'hidden'" class="md:hidden" id="navbar-default">
+        <ul class="flex flex-col mt-4">
+          <li>
+            <a href="#about" class="block py-2 px-4 text-center text-white hover:bg-primary-light">Who We Are</a>
+          </li>
+          <li>
+            <a href="#beers" class="block py-2 px-4 text-center text-white hover:bg-primary-light">Lager</a>
+          </li>
+          <li>
+            <a href="#story" class="block py-2 px-4 text-center text-white hover:bg-primary-light">Cream Ale</a>
+          </li>
+          <li>
+            <a href="#find" class="block py-2 px-4 text-center text-white hover:bg-primary-light">IPA</a>
+          </li>
+          <li>
+            <a href="#find" class="block py-2 px-4 text-center text-white hover:bg-primary-light">Storytelling</a>
+          </li>
+          <li>
+            <a href="#find" class="block py-2 px-4 text-center text-white hover:bg-primary-light">Ubication</a>
+          </li>
+        </ul>
+        <div class="flex items-center justify-center mt-4 space-x-4">
+          <NuxtLink href="#" class="text-white hover:text-primary flex items-center justify-center w-10 h-10">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+              <g fill="#ff8000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                style="mix-blend-mode: normal">
+                <g transform="scale(8,8)">
+                  <path
+                    d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z">
+                  </path>
+                </g>
+              </g>
+            </svg>
+          </NuxtLink>
+          <NuxtLink href="#" class="text-white hover:text-primary flex items-center justify-center w-7 h-7">
+            <svg fill="#ff8000" height="100%" width="100%" version="1.1" id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 310 310"
+              xml:space="preserve">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <g id="XMLID_834_">
+                  <path id="XMLID_835_"
+                    d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064 c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996 V71.978c0-9.732,5.24-14.667,15.576-14.667c1.473,0,29.42,0,29.42,0c2.762,0,5-2.239,5-5V5.037c0-2.762-2.238-5-5-5h-40.545 C187.467,0.023,186.832,0,185.896,0c-7.035,0-31.488,1.381-50.804,19.151c-21.402,19.692-18.427,43.27-17.716,47.358v37.752H81.703 c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z">
+                  </path>
+                </g>
+              </g>
+            </svg>
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+
 
 <script setup>
 import { ref } from 'vue';
@@ -52,12 +143,12 @@ const toggleMenu = () => {
 </script>
 
 <style scoped>
-/* Animación personalizada para deslizamiento desde la derecha */
 @keyframes slideIn {
   0% {
     transform: translateX(100%);
     opacity: 0;
   }
+
   100% {
     transform: translateX(0);
     opacity: 1;
---
+++ components/New.vue
@@ -0,0 +1,134 @@
+<template>
+    <div
+      class="flex items-center justify-center h-[60vh] bg-center bg-custom-size"
+      style="background-image: url('/images/mancha.webp'); background-repeat: no-repeat; background-position: center;"
+      data-aos="fade-up" 
+      data-aos-duration="1000"
+    >
+      <div class="w-full max-w-md p-8 bg-opacity-80 rounded-lg shadow-lg" data-aos="zoom-in" data-aos-duration="1000">
+        <h2 class="h2 font-bold text-center text-white mb-8">NEWSLETTER</h2>
+        <form @submit.prevent="handleSubmit">
+          <div class="mb-4">
+            <input
+              v-model="email"
+              type="email"
+              placeholder="Example@gmail.com"
+              class="w-full px-4 py-2 text-white bg-gray-900 rounded-md focus:outline-none 
+              focus:ring-2 focus:ring-orange-500 border-primary"
+              :class="{ 'border-red-500': errors.email }"
+              @input="validateEmail"
+              required
+            />
+            <p v-if="errors.email" class="text-red-500 text-sm mt-1">{{ errors.email }}</p>
+          </div>
+          <div class="flex items-center mb-4">
+            <input
+              v-model="isChecked"
+              type="checkbox"
+              id="promotions"
+              class="mr-2 text-orange-500 focus:ring-primary"
+              @change="validateCheckbox"
+            />
+            <label for="promotions" class="text-sm text-gray-300">
+              I want to receive emails and promotions
+            </label>
+          </div>
+          <p v-if="errors.checkbox" class="text-red-500 text-sm mb-2">{{ errors.checkbox }}</p>
+          <p class="text-center text-primary mb-4">contact@labru.com</p>
+          <div class="flex justify-center">
+            <button
+              type="submit"
+              class="w-1/2 py-2 rounded-full text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500"
+              :class="{ 'animate-pulse': isLoading, 'opacity-50 cursor-not-allowed': !isFormValid }"
+              :disabled="isLoading || !isFormValid"
+            >
+              <span v-if="isLoading">Sending...</span>
+              <span v-else>SEND</span>
+            </button>
+          </div>
+        </form>
+        <p v-if="isSubmitted" class="text-green-500 text-center mt-4">Thank you for subscribing!</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, computed } from 'vue'
+  
+  const email = ref('')
+  const isChecked = ref(false)
+  const isSubmitted = ref(false)
+  const isLoading = ref(false)
+  const errors = ref({ email: '', checkbox: '' })
+  
+  const isFormValid = computed(() => {
+    return email.value && isChecked.value && !errors.value.email && !errors.value.checkbox
+  })
+  
+  const validateEmail = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!email.value) {
+      errors.value.email = 'Email is required'
+    } else if (!emailPattern.test(email.value)) {
+      errors.value.email = 'Please enter a valid email address'
+    } else {
+      errors.value.email = ''
+    }
+  }
+  
+  const validateCheckbox = () => {
+    errors.value.checkbox = isChecked.value ? '' : 'You must accept to receive emails and promotions'
+  }
+  
+  const handleSubmit = () => {
+    validateEmail()
+    validateCheckbox()
+  
+    if (!isFormValid.value) {
+      return
+    }
+  
+    isLoading.value = true
+  
+    // Simular un envío de formulario (puedes reemplazarlo con tu lógica real)
+    setTimeout(() => {
+      console.log('Formulario enviado:', { email: email.value, isChecked: isChecked.value })
+      
+      // Limpiar solo el texto del campo de correo
+      email.value = '' // Esto vacía el input, pero mantiene el campo visible.
+      isChecked.value = false
+      errors.value = { email: '', checkbox: '' }
+      isSubmitted.value = true
+      isLoading.value = false
+    }, 2000)
+  }
+  </script>
+  
+  <style scoped>
+  /* Animación para el botón */
+  .animate-pulse {
+    animation: pulse 1s infinite;
+  }
+  .bg-custom-size {
+    background-size: 45%; /* Ajusta el tamaño como prefieras */
+  }
+  
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  
+  @media (max-width: 640px) { /* sm breakpoint */
+    .bg-custom-size {
+      background-size: 100%; /* Ajusta el tamaño como prefieras */
+    }
+  }
+  </style>
+  
